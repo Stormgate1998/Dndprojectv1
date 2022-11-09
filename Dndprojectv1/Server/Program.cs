44bf768fdbc4651fd379018fe4e7a6aea608849d
garv5014 <garv5014@gmail.com>
2022-11-08 21:29:50 -0700

    added server directive

--- Dndprojectv1/Server/Program.cs
+++ Dndprojectv1/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+builder.Services.AddDbContext<DNDDbContext>(options =>
+    options.UseSqlite("data source = DNDServer"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
